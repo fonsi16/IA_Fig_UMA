4e1eefc52612fb15bb95b64b9b4856deb6e8b110
afons <mikerodri1602@gmail.com>
2023-11-25 17:23:02 +0000

    Mudanças a nivel de código e robo ja insere os objetos na matriz

--- matriz.py
+++ matriz.py
@@ -13,11 +13,11 @@ def cria_matriz(tamanho):
     for i in range(tamanho):
         matriz.append([])
         for z in range(tamanho):
-            matriz[i].append("")
+            matriz[i].append(" ")
     return matriz
 
 def imprime_matriz(matriz):
-    print("-="*5+"MATRIZ JOGO"+"-="*5)
+    print("-="*3+"MATRIZ JOGO"+"-="*2)
     for i in range(len(matriz)):
         linha = "|"
         for j in range(len(matriz[i])):
@@ -27,22 +27,9 @@ def imprime_matriz(matriz):
     
 def inserir_objeto_matriz(objeto,linha,coluna,matriz):
     matriz[linha][coluna] =objeto
-
-
-#tem de receber o objeto também para então mandar para o 'inserir_objeto_metriz' pq não vai ser sempre 'x'
-#aqui ele coloca o objeto no primeiro espaço vazio que houver na matriz
-def verifica_tem_objeto(matriz, objeto):
-    vazio = True
-    for i in range(len(matriz)):
-        if(vazio == True):
-            for j in range(len(matriz[i])):
-                    if matriz[i][j] == "" :
-                        inserir_objeto_matriz(objeto,i+1,j+1,matriz)
-                        vazio = False
-                        break
         
 def verifica_vazio(matriz, linha, coluna):
-    if matriz[linha][coluna] == "":
+    if matriz[linha][coluna] == " ":
         return True
     else:
         return False
